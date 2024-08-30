d4f6e138dae678ab338000149095a3cea453dfec
Eddie-Pak <foalstn@naver.com>
2024-08-31 04:05:20 +0900

    뉴스 리스트 UI 구현-1

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newsapp.databinding.ActivityMainBinding
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
 import retrofit2.Call
@@ -11,6 +13,8 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var newsAdapter: NewsAdapter
 
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://news.google.com/")
@@ -24,12 +28,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        newsAdapter = NewsAdapter()
+
+        binding.newsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = newsAdapter
+        }
 
         val newsService = retrofit.create(NewsService::class.java)
         newsService.mainFeed().enqueue(object: Callback<NewsRss> {
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
                 Log.e("MainActivity", "${response.body()?.channel?.items}")
+
+                newsAdapter.submitList(response.body()?.channel?.items.orEmpty())
             }
 
             override fun onFailure(call: Call<NewsRss>, t: Throwable) {
